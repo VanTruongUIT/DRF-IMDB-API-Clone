279a491f79cf1b3722f4c578aa46f72078a2bcdc
VanTruongUIT <tvtruong.uit@gmail.com>
2022-05-23 00:15:56 +0700

    Using the Model Serializer to implement the drf

--- watch/movies/api/serializers.py
+++ watch/movies/api/serializers.py
@@ -1,4 +1,5 @@
 from wsgiref.validate import validator
+from xml.parsers.expat import model
 from django.forms import IntegerField
 from rest_framework import serializers
 
@@ -8,23 +9,10 @@ from movies.models import Movies
 def validate_length_of_field(value):
     if len(value) < 2:
         raise serializers.ValidationError("Length of field too short!!!")
-class MoviesSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    movie_name = serializers.CharField()
-    description = serializers.CharField(validators=[validate_length_of_field, ])
-    active = serializers.BooleanField()
-    
-    
-    def create(self, validated_data):
-        return Movies.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.movie_name = validated_data.get("movie_name", instance.movie_name)
-        instance.description = validated_data.get("description", instance.description)
-        instance.active = validated_data.get("active", instance.active)
-        instance.save()
-        return instance
-    
+class MoviesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movies 
+        fields = "__all__"        
     # Field level validation function start with validate_<field_name> look like below
     def validate_movie_name(self, value):
         if len(value) < 2:
