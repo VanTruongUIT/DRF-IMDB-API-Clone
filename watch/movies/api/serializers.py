896d363fb9bca408b21b9c931baa3f08710f8bc0
VanTruongUIT <tvtruong.uit@gmail.com>
2022-05-23 00:00:31 +0700

    add some validation in serializer class

--- watch/movies/api/serializers.py
+++ watch/movies/api/serializers.py
@@ -1,13 +1,17 @@
+from wsgiref.validate import validator
 from django.forms import IntegerField
 from rest_framework import serializers
 
 from movies.models import Movies
 
 
+def validate_length_of_field(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Length of field too short!!!")
 class MoviesSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     movie_name = serializers.CharField()
-    description = serializers.CharField()
+    description = serializers.CharField(validators=[validate_length_of_field, ])
     active = serializers.BooleanField()
     
     
@@ -20,3 +24,17 @@ class MoviesSerializer(serializers.Serializer):
         instance.active = validated_data.get("active", instance.active)
         instance.save()
         return instance
+    
+    # Field level validation function start with validate_<field_name> look like below
+    def validate_movie_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("The movie name is too short!!!")
+        else:
+            return value 
+        
+    # Validate with object-level
+    def validate(self, attrs):
+        if (attrs.get('movie_name') == attrs.get('description')):
+            raise serializers.ValidationError("The movie name and description should not be same!!!")
+        else:
+            return attrs
