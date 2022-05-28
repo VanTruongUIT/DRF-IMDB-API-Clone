bb61c244be1b6735437439a13a1bdcfe656d6623
VanTruongUIT <tvtruong.uit@gmail.com>
2022-05-28 22:13:10 +0700

    modify the url file

--- watch/movies/api/urls.py
+++ watch/movies/api/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     path("list/<int:pk>", WatchListDetail.as_view(), name="watchlist-detail"),
     path("stream-platforms/", StreamFlatformList.as_view(), name="streamplatform-list"),
     path("stream-platforms/<int:pk>", StreamPlatformDetail.as_view(), name="streamplatform-detail"),
-    path("list/reviews/", ReviewList.as_view(), name="review-list"),
-    path("list/<int:pk>/reviews/", ReviewDetail.as_view(), name="review-detail"),
+    path("reviews/", ReviewList.as_view(), name="review-list"),
+    path("reviews/<int:pk>", ReviewDetail.as_view(), name="review-detail"),
 ]
 
 
