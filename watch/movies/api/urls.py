ca15733f6c5e0898c76a82c051ccfde426867b57
VanTruongUIT <tvtruong.uit@gmail.com>
2022-06-17 00:54:38 +0700

    create endpoint to create token using the obtain_auth_token is provided by rest_framework

--- watch/movies/api/urls.py
+++ watch/movies/api/urls.py
@@ -2,6 +2,7 @@ from django.db import router
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 # from .views import get_all_movies, get_detail_movies
 from .views import (
@@ -20,7 +21,7 @@ router.register("stream-platforms", StreamPlatformViewSet, basename="streamplatf
 
 urlpatterns = [
     path("list/", WatchListList.as_view(), name="watchlist-list"),
-    path("list/<int:pk>", WatchListDetail.as_view(), name="watchlist-detail"),
+    path("list/<int:pk>/", WatchListDetail.as_view(), name="watchlist-detail"),
     
     path("", include(router.urls)),
     # path("stream-platforms/", StreamFlatformList.as_view(), name="streamplatform-list"),
@@ -30,9 +31,12 @@ urlpatterns = [
     # path("reviews/<int:pk>", ReviewDetail.as_view(), name="review-detail"),
     path("<int:pk>/reviews-create/", ReviewCreate.as_view(), name="review-create"),
     path("<int:pk>/reviews/", ReviewList.as_view(), name="review-list"),
-    path("reviews/<int:pk>", ReviewDetail.as_view(), name="review-detail"),
+    path("reviews/<int:pk>/", ReviewDetail.as_view(), name="review-detail"),
     
 ]
 
+urlpatterns += [
+    path('api-token-auth/', views.obtain_auth_token)
+]
 
 
--- watch/watch/settings.py
+++ watch/watch/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies',
+    'users',
     'rest_framework',
     'rest_framework.authtoken',
 ]
