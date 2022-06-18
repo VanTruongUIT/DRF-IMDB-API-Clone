71b1a35adaf112f60af739921776744334188cee
VanTruongUIT <tvtruong.uit@gmail.com>
2022-06-18 17:52:32 +0700

    create and response token after register user

--- watch/users/api/views.py
+++ watch/users/api/views.py
@@ -1,6 +1,9 @@
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from .serializers import RegistrationSerializer
 from rest_framework.response import Response
+
+# from users import models
 
 
 @api_view(["POST", ])
@@ -8,7 +11,21 @@ def register_view(request):
     
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+        print(f'serializer {serializer}')
         
+        data = {}
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            user = serializer.save()
+            
+            data['response'] = 'Registration successful!'
+            data['username'] = user.username 
+            
+            token = Token.objects.get(user=user).key
+            data['token'] = token 
+            
+            
+        else:
+            data = serializer.errors
+            
+        print(data)
+        return Response(data)
